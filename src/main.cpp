a9bd9a2670498819af54a1142d5e7b410e7dee08
SoF4rAway <muhammadfaridrahman.mfr@gmail.com>
2024-06-14 08:03:46 +0700

    Updates

--- src/main.cpp
+++ src/main.cpp
@@ -1,92 +1,64 @@
-#include <Arduino.h>
+// Include the AccelStepper Library
+#include <AccelStepper.h>
 #include <Wire.h>
 #include <uRTCLib.h>
-
-uRTCLib rtc(0x68);
+#include <NewPing.h>
+#include <DHT.h>
 
 unsigned char low_data[8] = {0};
 unsigned char high_data[12] = {0};
 
-#define NO_TOUCH 0xFE
-#define THRESHOLD 100
-#define ATTINY1_HIGH_ADDR 0x78
-#define ATTINY2_LOW_ADDR 0x77
+#define NO_TOUCH       0xFE
+#define THRESHOLD      100
+#define ATTINY1_HIGH_ADDR   0x78
+#define ATTINY2_LOW_ADDR   0x77
+
+
+#define DHT11_PIN 13
+
+DHT dht11(DHT11_PIN, DHT11);
 
 // Define pin connections
 const int dirPin = 6;
 const int stepPin = 5;
 int day, hour, minute, dayOfWeek, w_lv_value, food_lv;
-int lastDayChecked = -1;
-int lastHourChecked = -1;
-
-// Include the AccelStepper Library
-#include <AccelStepper.h>
 
 // Define motor interface type
 #define motorInterfaceType 1
 
-AccelStepper myStepper(motorInterfaceType, stepPin, dirPin);
-
-#include <NewPing.h>
+uRTCLib rtc(0x68);
 NewPing sonar(7, 8, 20);
-
-#include <DHT.h>
-
-#define DHT11_PIN 13
-
-DHT dht11(DHT11_PIN, DHT11);
-float humi, temp;
-
-void readTempHumi(float &humi, float &temp)
-{
-    humi = dht11.readHumidity();
-    temp = dht11.readTemperature();
-    Serial.println("Temp and Humidity = ");
-    Serial.print("Temperature : ");
-    Serial.print(temp);
-    Serial.print("\xC2\xB0");
-    Serial.print("C");
-
-    Serial.print(" | ");
-    Serial.print("Humidity : ");
-    Serial.print(humi);
-    Serial.print("&");
-}
 
 void getHigh12SectionValue(void)
 {
-    memset(high_data, 0, sizeof(high_data));
-    Wire.requestFrom(ATTINY1_HIGH_ADDR, 12);
-    while (12 != Wire.available())
-        ;
+  memset(high_data, 0, sizeof(high_data));
+  Wire.requestFrom(ATTINY1_HIGH_ADDR, 12);
+  while (12 != Wire.available());
 
-    for (int i = 0; i < 12; i++)
-    {
-        high_data[i] = Wire.read();
-    }
-    delay(10);
+  for (int i = 0; i < 12; i++) {
+    high_data[i] = Wire.read();
+  }
+  delay(10);
 }
 
 void getLow8SectionValue(void)
 {
-    memset(low_data, 0, sizeof(low_data));
-    Wire.requestFrom(ATTINY2_LOW_ADDR, 8);
-    while (8 != Wire.available())
-        ;
+  memset(low_data, 0, sizeof(low_data));
+  Wire.requestFrom(ATTINY2_LOW_ADDR, 8);
+  while (8 != Wire.available());
 
-    for (int i = 0; i < 8; i++)
-    {
-        low_data[i] = Wire.read(); // receive a byte as character
-    }
-    delay(10);
+  for (int i = 0; i < 8 ; i++) {
+    low_data[i] = Wire.read(); // receive a byte as character
+  }
+  delay(10);
 }
 
-void check(int &w_lv_value)
+void check()
 {
-    int sensorvalue_min = 250;
-    int sensorvalue_max = 255;
-    int low_count = 0;
-    int high_count = 0;
+  int sensorvalue_min = 250;
+  int sensorvalue_max = 255;
+  int low_count = 0;
+  int high_count = 0;
     uint32_t touch_val = 0;
     uint8_t trig_section = 0;
     low_count = 0;
@@ -97,138 +69,132 @@ void check(int &w_lv_value)
     Serial.println("low 8 sections value = ");
     for (int i = 0; i < 8; i++)
     {
-        Serial.print(low_data[i]);
-        Serial.print(".");
-        if (low_data[i] >= sensorvalue_min && low_data[i] <= sensorvalue_max)
-        {
-            low_count++;
-        }
-        if (low_count == 8)
-        {
-            Serial.print("      ");
-            Serial.print("PASS");
-        }
+      Serial.print(low_data[i]);
+      Serial.print(".");
+      if (low_data[i] >= sensorvalue_min && low_data[i] <= sensorvalue_max)
+      {
+        low_count++;
+      }
+      if (low_count == 8)
+      {
+        Serial.print("      ");
+        Serial.print("PASS");
+      }
     }
     Serial.println("  ");
     Serial.println("  ");
     Serial.println("high 12 sections value = ");
     for (int i = 0; i < 12; i++)
     {
-        Serial.print(high_data[i]);
-        Serial.print(".");
+      Serial.print(high_data[i]);
+      Serial.print(".");
 
-        if (high_data[i] >= sensorvalue_min && high_data[i] <= sensorvalue_max)
-        {
-            high_count++;
-        }
-        if (high_count == 12)
-        {
-            Serial.print("      ");
-            Serial.print("PASS");
-        }
+      if (high_data[i] >= sensorvalue_min && high_data[i] <= sensorvalue_max)
+      {
+        high_count++;
+      }
+      if (high_count == 12)
+      {
+        Serial.print("      ");
+        Serial.print("PASS");
+      }
     }
 
     Serial.println("  ");
     Serial.println("  ");
 
-    for (int i = 0; i < 8; i++)
-    {
-        if (low_data[i] > THRESHOLD)
-        {
-            touch_val |= 1 << i;
-        }
+    for (int i = 0 ; i < 8; i++) {
+      if (low_data[i] > THRESHOLD) {
+        touch_val |= 1 << i;
+
+      }
     }
-    for (int i = 0; i < 12; i++)
-    {
-        if (high_data[i] > THRESHOLD)
-        {
-            touch_val |= (uint32_t)1 << (8 + i);
-        }
+    for (int i = 0 ; i < 12; i++) {
+      if (high_data[i] > THRESHOLD) {
+        touch_val |= (uint32_t)1 << (8 + i);
+      }
     }
 
     while (touch_val & 0x01)
     {
-        trig_section++;
-        touch_val >>= 1;
+      trig_section++;
+      touch_val >>= 1;
     }
-    w_lv_value = trig_section *5 ;
     Serial.print("water level = ");
-    Serial.print(w_lv_value);
+    Serial.print(trig_section * 5);
     Serial.println("% ");
     Serial.println(" ");
     Serial.println("*********************************************************");
     delay(1000);
 }
 
-void moveStepper()
-{
-    myStepper.moveTo(800);     // Set desired move: 800 steps (in quater-step resolution that's one rotation)
-    myStepper.runToPosition(); // Moves the motor to target position w/ acceleration/ deceleration and it blocks until is in position
+// Creates an instance
+AccelStepper myStepper(motorInterfaceType, stepPin, dirPin);
+
+void moveStepper(){
+  myStepper.moveTo(-9600); // Set desired move: 800 steps (in quater-step resolution that's one rotation)
+  myStepper.runToPosition(); // Moves the motor to target position w/ acceleration/ deceleration and it blocks until is in position
 }
-void moveStepperBack()
-{
-    myStepper.moveTo(0);
-    myStepper.runToPosition();
+void moveStepperBack(){
+  myStepper.moveTo(0);
+  myStepper.runToPosition();
 }
 
-void readFood(int &food_lv)
+void readFood(int &food_lv){
+  int val;
+  val = sonar.ping_cm();
+  food_lv = map(val,0,20,100,0);
+  Serial.println("Food Level");
+  Serial.print(food_lv);
+  Serial.print("%");
+}
+
+void readTempHumi(float &humi, float &temp)
 {
-    int val;
-    val = sonar.ping_cm();
-    food_lv = map(val, 0, 20, 100, 0);
-    Serial.println("Food Level");
-    Serial.print(food_lv);
-    Serial.print("%");
+    humi = dht11.readHumidity();
+    temp = dht11.readTemperature();
 }
 
 void rtcRead(int &dayOfWeek, int &day, int &hour, int &minute)
 {
-    rtc.refresh();
-    dayOfWeek = rtc.dayOfWeek();
-    day = rtc.day();
-    hour = rtc.hour();
-    minute = rtc.minute();
-    Serial.print("Current Date & Time: ");
+  rtc.refresh();
+  dayOfWeek = rtc.dayOfWeek();
+  day = rtc.day();
+  hour = rtc.hour();
+  minute = rtc.minute();
+  Serial.print("Current Date & Time: ");
 
-    Serial.print(day);
-    Serial.print(" (");
-    Serial.print(dayOfWeek);
-    Serial.print(") ");
+  Serial.print(day);
+  Serial.print(" (");
+  Serial.print(dayOfWeek);
+  Serial.print(") ");
 
-    Serial.print(hour);
-    Serial.print(':');
-    Serial.print(minute);
-    Serial.println();
+  Serial.print(hour);
+  Serial.print(':');
+  Serial.print(minute);
+  Serial.println();
 }
 
-void setup()
-{
-    // set the maximum speed, acceleration factor,
-    // initial speed and the target position
-    dht11.begin();
-    Serial.begin(115200);
-    URTCLIB_WIRE.begin();
-    Wire.begin();
-    rtc.set(0, 59, 15, 3, 28, 2, 24);
-    myStepper.setMaxSpeed(1000);
-    myStepper.setAcceleration(50);
-    myStepper.setSpeed(200);
-    myStepper.setCurrentPosition(0);
+void setup() {
+	// set the maximum speed, acceleration factor,
+	// initial speed and the target position
+  Serial.begin(115200);
+  URTCLIB_WIRE.begin();
+  Wire.begin();
+  dht11.begin();
+  rtc.set(0, 6, 14, 3, 28, 2, 24);
+	myStepper.setMaxSpeed(1000);
+	myStepper.setAcceleration(50);
+	myStepper.setSpeed(200);
+  myStepper.setCurrentPosition(0);
 }
 
-void loop()
-{
-    rtcRead(dayOfWeek, day, hour, minute);
-    readTempHumi(humi, temp);
-    check(w_lv_value);
-    readFood(food_lv);
-    if (hour != lastHourChecked){
-        lastHourChecked = hour;
-        if(lastDayChecked != int(day) && int(hour) == 16){
-            lastDayChecked = day;
-            moveStepper();
-            moveStepperBack();
-        }
-    }
+void loop() {
+
+  moveStepper();
+  rtcRead(dayOfWeek, day, hour, minute);
+  check();
+  readFood(food_lv);
+  moveStepperBack();
 
 }
--- t.cpp
+++ t.cpp
@@ -1,272 +1,223 @@
 #include <Arduino.h>
-#include <Firebase_ESP_Client.h>
-#include <addons/TokenHelper.h>
-#include <NTPClient.h>
-#include <WiFiUdp.h>
-#include <TimeLib.h>
-// Insert your network credentials
-#define WIFI_SSID "_"
-#define WIFI_PASSWORD "jwtl1810"
+#include <Wire.h>
+#include <uRTCLib.h>
 
-// Insert Firebase project API Key
-#define API_KEY "AIzaSyArADf57SHliMuSiU7mf1ILITMf2pyBa6k"
-#define FIREBASE_PROJECT_ID "tugasakhir-c7b7b"
-// Insert RTDB URLefine the RTDB URL */
+uRTCLib rtc(0x68);
 
-#define USER_EMAIL "esp32@esp.com"
-#define USER_PASSWORD "taini123"
+unsigned char low_data[8] = {0};
+unsigned char high_data[12] = {0};
 
-//server mengambil data tanggal dan waktu sebenarnya
-#define NTP_SERVER "time.nist.gov"
-WiFiUDP ntpUDP;
-NTPClient timeClient(ntpUDP, NTP_SERVER);
+#define NO_TOUCH 0xFE
+#define THRESHOLD 100
+#define ATTINY1_HIGH_ADDR 0x78
+#define ATTINY2_LOW_ADDR 0x77
 
+// Define pin connections
+const int dirPin = 6;
+const int stepPin = 5;
+int day, hour, minute, dayOfWeek, w_lv_value, food_lv;
+int lastDayChecked = -1;
+int lastHourChecked = -1;
 
-#define INTERVAL 2000 // Delay between sensor readings in milliseconds
-#define UPDATE_INTERVAL 60000 // Delay between data updates in milliseconds (10 seconds)
-#define UPLOAD_INTERVAL 60000 // Delay between data uploads in milliseconds (1 minute)
+// Include the AccelStepper Library
+#include <AccelStepper.h>
 
-#define pH_PIN A0 //inisialisasi pH
-#define Offset 0.00
-#define samplingInterval 20
-#define printInterval 800
-#define ArrayLenth  40
-int pHArray[ArrayLenth];
-int pHArrayIndex=0;
-static unsigned long samplingTime = 0;
-static unsigned long printTime = 0;
-float pHValue,voltage;
+// Define motor interface type
+#define motorInterfaceType 1
 
-#define DO_PIN A1 //Inisialisasi DO
-#define VREF 5000    // VREF (mv)
-#define ADC_RES 1024 // ADC Resolution
-#define TWO_POINT_CALIBRATION 0
-#define READ_TEMP (25)
-#define CAL1_V (1600)
-#define CAL1_T (25)
-#define CAL2_V (1300)
-#define CAL2_T (15)
-const uint16_t DO_Table[41] = {14460, 14220, 13820, 13440, 13090, 12740, 12420, 12110, 11810, 11530,
-                                11260, 11010, 10770, 10530, 10300, 10080, 9860, 9660, 9460, 9270,
-                                9080, 8900, 8730, 8570, 8410, 8250, 8110, 7960, 7820, 7690,
-                                7560, 7430, 7300, 7180, 7070, 6950, 6840, 6730, 6630, 6530, 6410};
+AccelStepper myStepper(motorInterfaceType, stepPin, dirPin);
 
-uint8_t Temperaturet;
-uint16_t ADC_Raw;
-uint16_t ADC_Voltage;
-uint16_t DO;
+#include <NewPing.h>
+NewPing sonar(7, 8, 20);
 
-//Define Firebase Data object
-FirebaseData fbdo;
-FirebaseAuth auth;
-FirebaseConfig config;
+#include <DHT.h>
 
-unsigned long prevUpdateMillis = 0;
-unsigned long prevUploadMillis = 0;
-int count = 0;
-bool signupOK = false;
+#define DHT11_PIN 13
 
-String getTanggalWaktu() {
-    timeClient.update();
-    unsigned long rawTime = timeClient.getEpochTime();
-    time_t t = rawTime + (7 * 3600);
+DHT dht11(DHT11_PIN, DHT11);
+float humi, temp;
 
-    int jam = hour(t);
-    String jamStr = jam < 10 ? "0" + String(jam) : String(jam);
-
-    int menit = minute(t);
-    String menitStr = menit < 10 ? "0" + String(menit) : String(menit);
-
-    int detik = second(t);
-    String detikStr = detik < 10 ? "0" + String(detik) : String(detik);
-
-    int tgl = day(t);
-    String tglStr = tgl < 10 ? "0" + String(tgl) : String(tgl);
-
-    int bln = month(t);
-    String blnStr = bln < 10 ? "0" + String(bln) : String(bln);
-
-    int thn = year(t);
-    String thnStr = String(thn);
-
-    String tanggal = tglStr + "/" + blnStr + "/" + thnStr;
-    String waktu = jamStr + ":" + menitStr + ":" + detikStr;
-
-    return tanggal + " " + waktu;
-}
-
-void updateData(FirebaseData& fbdo, float pHValue, float DO)
+void readTempHumi(float &humi, float &temp)
 {
-     if (WiFi.status() == WL_CONNECTED && Firebase.ready())
-    {
-        String tanggalWaktu = getTanggalWaktu();
-        FirebaseJson data;
-        
-        String documentPath = "karamba/"+ tanggalWaktu.substring(6, 10) + "-" + tanggalWaktu.substring(3, 5) + "-" + tanggalWaktu.substring(0, 2);
-        String mask = "pH,DO";
-        
-        data.set("fields/Tanggal/stringValue", tanggalWaktu.substring(0, 10));
-        data.set("fields/pH/doubleValue", pHValue);
-        data.set("fields/DO/doubleValue", DO);
+    humi = dht11.readHumidity();
+    temp = dht11.readTemperature();
+    Serial.println("Temp and Humidity = ");
+    Serial.print("Temperature : ");
+    Serial.print(temp);
+    Serial.print("\xC2\xB0");
+    Serial.print("C");
 
-        if (Firebase.Firestore.patchDocument(&fbdo, FIREBASE_PROJECT_ID, "", documentPath.c_str(), data.raw(), mask.c_str()))
-        {
-             Serial.printf("ok\n%s\n\n", fbdo.payload().c_str());
-        }
-        else
-        {
-            Serial.println(fbdo.errorReason());
-        }
-    }
+    Serial.print(" | ");
+    Serial.print("Humidity : ");
+    Serial.print(humi);
+    Serial.print("&");
 }
 
-void uploadData(FirebaseData& fbdo, float pHValue, float DO)
+void getHigh12SectionValue(void)
 {
-    if (WiFi.status() == WL_CONNECTED && Firebase.ready())
+    memset(high_data, 0, sizeof(high_data));
+    Wire.requestFrom(ATTINY1_HIGH_ADDR, 12);
+    while (12 != Wire.available())
+        ;
+
+    for (int i = 0; i < 12; i++)
     {
-        String tanggalWaktu = getTanggalWaktu(); // Mendapatkan tanggal dan waktu sebenarnya
-        
-        FirebaseJson data;
-       
-        String documentPath = "history/" + tanggalWaktu.substring(6, 10) + "-" + tanggalWaktu.substring(3, 5) + "-" + tanggalWaktu.substring(0, 2) + "/" + tanggalWaktu.substring(11, 19);
-      
-        data.set("fields/Tanggal/stringValue", tanggalWaktu.substring(0, 10));
-        data.set("fields/Jam/stringValue", tanggalWaktu.substring(11));
-        data.set("fields/pH/doubleValue", pHValue);
-        data.set("fields/DO/doubleValue", DO);
-        
-        if (Firebase.Firestore.createDocument(&fbdo, FIREBASE_PROJECT_ID, "", documentPath.c_str(), data.raw())) {
-            Serial.print("ok\n%s\n\n");
-            Serial.println(fbdo.payload());
-        } else {
-            Serial.println("Gagal mengirim data ke Firestore");
-            Serial.println(fbdo.errorReason());
-        }  
+        high_data[i] = Wire.read();
     }
+    delay(10);
 }
 
-void setup(){
-  Serial.begin(115200);
-  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-  Serial.print("Connecting to Wi-Fi");
-  while (WiFi.status() != WL_CONNECTED){
-    Serial.print(".");
-    delay(300);
-  }
-  Serial.println();
-  Serial.print("Connected with IP: ");
-  Serial.println(WiFi.localIP());
-  Serial.println();
+void getLow8SectionValue(void)
+{
+    memset(low_data, 0, sizeof(low_data));
+    Wire.requestFrom(ATTINY2_LOW_ADDR, 8);
+    while (8 != Wire.available())
+        ;
 
-  /* Assign the api key (required) */
-  config.api_key = API_KEY; // Mengatur API key Firebase
-  auth.user.email = USER_EMAIL;
-  auth.user.password = USER_PASSWORD;
-  config.token_status_callback = tokenStatusCallback;
-
-  /* Assign the callback function for the long running token generation task */
-  fbdo.setResponseSize(4096);
-  Firebase.begin(&config, &auth);
-  Firebase.reconnectWiFi(true);
-  timeClient.begin();
+    for (int i = 0; i < 8; i++)
+    {
+        low_data[i] = Wire.read(); // receive a byte as character
+    }
+    delay(10);
 }
 
-void loop(){
-  unsigned long currentTime = millis();
-  
-  if(millis()-samplingTime > samplingInterval) {
-    pHArray[pHArrayIndex++]=analogRead(pH_PIN);
-    if(pHArrayIndex==ArrayLenth) pHArrayIndex=0;
-    voltage = avergearray(pHArray, ArrayLenth)*5.0/1023;
-    pHValue = 3.5*voltage+Offset;
-    samplingTime=millis();
+void check(int &w_lv_value)
+{
+    int sensorvalue_min = 250;
+    int sensorvalue_max = 255;
+    int low_count = 0;
+    int high_count = 0;
+    uint32_t touch_val = 0;
+    uint8_t trig_section = 0;
+    low_count = 0;
+    high_count = 0;
+    getLow8SectionValue();
+    getHigh12SectionValue();
 
-    Temperaturet = (uint8_t)READ_TEMP;
-    ADC_Raw = analogRead(DO_PIN);
-    ADC_Voltage = (uint32_t)VREF * ADC_Raw / ADC_RES;
-
-    Serial.print("pH: ");
-    Serial.println(pHValue, 2);
-
-    Serial.print("Temperature: ");
-    Serial.println(Temperaturet);
-
-    DO = readDO(ADC_Voltage, Temperaturet);
-    Serial.print("DO: ");
-    Serial.println(DO);
-  }
-
-  if(millis() - printTime > printInterval) {
-    Serial.print("Voltage: ");
-    Serial.print(voltage, 2);
-    Serial.print("    pH value: ");
-    Serial.println(pHValue, 2);
-    printTime=millis();
-  }
-
-// Pengiriman data ke monitoring
-  if (currentTime - prevUpdateMillis >= UPDATE_INTERVAL) {
-    updateData(fbdo, pHValue, DO);
-    prevUpdateMillis = currentTime;
-  }
-
-  // Pengiriman data ke histori
-  if (currentTime - prevUploadMillis >= UPLOAD_INTERVAL) {
-    uploadData(fbdo, pHValue, DO);
-    prevUploadMillis = currentTime;
-  }
-  
-  delay (INTERVAL);
-}
-
-double avergearray(int* arr, int number) {
-  int i;
-  int max, min;
-  double avg;
-  long amount=0;
-
-  if(number<=0){
-    Serial.println("Error number for the array to avraging!");
-    return 0;
-  }
-
-  if(number<5){
-    for(i=0;i<number;i++){
-      amount+=arr[i];
-    }
-    avg = amount/number;
-    return avg;
-  } else {
-    if(arr[0]<arr[1]){
-      min = arr[0];max=arr[1];
-    } else {
-      min=arr[1];max=arr[0];
-    }
-
-    for(i=2;i<number;i++){
-      if(arr[i]<min){
-        amount+=min;
-        min=arr[i];
-      } else {
-        if(arr[i]>max){
-          amount+=max;
-          max=arr[i];
-        } else {
-          amount+=arr[i];
+    Serial.println("low 8 sections value = ");
+    for (int i = 0; i < 8; i++)
+    {
+        Serial.print(low_data[i]);
+        Serial.print(".");
+        if (low_data[i] >= sensorvalue_min && low_data[i] <= sensorvalue_max)
+        {
+            low_count++;
         }
-      }
+        if (low_count == 8)
+        {
+            Serial.print("      ");
+            Serial.print("PASS");
+        }
     }
-    avg = (double)amount/(number-2);
-  }
-  return avg;
+    Serial.println("  ");
+    Serial.println("  ");
+    Serial.println("high 12 sections value = ");
+    for (int i = 0; i < 12; i++)
+    {
+        Serial.print(high_data[i]);
+        Serial.print(".");
+
+        if (high_data[i] >= sensorvalue_min && high_data[i] <= sensorvalue_max)
+        {
+            high_count++;
+        }
+        if (high_count == 12)
+        {
+            Serial.print("      ");
+            Serial.print("PASS");
+        }
+    }
+
+    Serial.println("  ");
+    Serial.println("  ");
+
+    for (int i = 0; i < 8; i++)
+    {
+        if (low_data[i] > THRESHOLD)
+        {
+            touch_val |= 1 << i;
+        }
+    }
+    for (int i = 0; i < 12; i++)
+    {
+        if (high_data[i] > THRESHOLD)
+        {
+            touch_val |= (uint32_t)1 << (8 + i);
+        }
+    }
+
+    while (touch_val & 0x01)
+    {
+        trig_section++;
+        touch_val >>= 1;
+    }
+    w_lv_value = trig_section *5 ;
+    Serial.print("water level = ");
+    Serial.print(w_lv_value);
+    Serial.println("% ");
+    Serial.println(" ");
+    Serial.println("*********************************************************");
+    delay(1000);
 }
 
-int16_t readDO(uint32_t voltage_mv, uint8_t temperature_c) {
-#if TWO_POINT_CALIBRATION == 0
-  uint16_t V_saturation = (uint32_t)CAL1_V + (uint32_t)35 * temperature_c - (uint32_t)CAL1_T * 35;
-  return (voltage_mv * DO_Table[temperature_c] / V_saturation);
-#else
-  uint16_t V_saturation = (int16_t)((int8_t)temperature_c - CAL2_T) * ((uint16_t)CAL1_V - CAL2_V) / ((uint8_t)CAL1_T - CAL2_T) + CAL2_V;
-  return (voltage_mv * DO_Table[temperature_c] / V_saturation);
-#endif
+void moveStepper()
+{
+    myStepper.moveTo(12800);     // Set desired move: 800 steps (in quater-step resolution that's one rotation)
+    myStepper.runToPosition(); // Moves the motor to target position w/ acceleration/ deceleration and it blocks until is in position
+}
+void moveStepperBack()
+{
+    myStepper.moveTo(0);
+    myStepper.runToPosition();
+}
+
+void readFood(int &food_lv)
+{
+    int val;
+    val = sonar.ping_cm();
+    food_lv = map(val, 0, 20, 100, 0);
+    Serial.println("Food Level");
+    Serial.print(food_lv);
+    Serial.print("%");
+}
+
+void rtcRead(int &dayOfWeek, int &day, int &hour, int &minute)
+{
+    rtc.refresh();
+    dayOfWeek = rtc.dayOfWeek();
+    day = rtc.day();
+    hour = rtc.hour();
+    minute = rtc.minute();
+    Serial.print("Current Date & Time: ");
+
+    Serial.print(day);
+    Serial.print(" (");
+    Serial.print(dayOfWeek);
+    Serial.print(") ");
+
+    Serial.print(hour);
+    Serial.print(':');
+    Serial.print(minute);
+    Serial.println();
+}
+
+void setup()
+{
+    // set the maximum speed, acceleration factor,
+    // initial speed and the target position
+    dht11.begin();
+    Serial.begin(115200);
+    URTCLIB_WIRE.begin();
+    Wire.begin();
+    rtc.set(0, 59, 15, 3, 28, 2, 24);
+    myStepper.setMaxSpeed(1000);
+    myStepper.setAcceleration(50);
+    myStepper.setSpeed(200);
+    myStepper.setCurrentPosition(0);
+}
+
+void loop()
+{
+    moveStepper();
+    moveStepperBack();
 }
